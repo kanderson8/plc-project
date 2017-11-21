7863c5f4f6ce0a00751f32b7a026ac5e2607726c
Cole Pierce <31912242+colemtg@users.noreply.github.com>
2017-11-21 16:52:55 -0600

    Converted to type (Int, Vertex, Vertex) for E

--- Graph.hs
+++ Graph.hs
@@ -1,10 +1,33 @@
 module Graph where
 import qualified Data.ByteString.Char8 as C
+import Data.Maybe
 
 type V = C.ByteString
 
-type E = (C.ByteString,V,V)
+type E = (Int,V,V)
+
+type Ein = (C.ByteString,V,V)
 
 data UndirectedGraph = UndirectedGraph [V] [E]
   deriving (Show)
+
+
+eToEin :: [Ein] -> [E]
+eToEin (e:es) = (convertToInt e) : (eToEin es)
+eToEin [] = []
+
+convertToInt :: Ein -> E
+convertToInt (char, a, b) = ((charToInt char) , a , b)
+
+charToInt :: C.ByteString -> Int
+charToInt c = fromMaybeIntChar (C.readInt c)
+
+fromMaybeIntChar :: Maybe (Int, C.ByteString) ->Int
+fromMaybeIntChar Nothing = 0
+fromMaybeIntChar (Just a) = fromTuple (fromJust (Just a))
+
+fromTuple :: (Int,C.ByteString) -> Int
+fromTuple (a,b) = a
+
+
 
