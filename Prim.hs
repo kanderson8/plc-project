da364018c340cb0f0c9871f32b3255e368f2a1a2
Cole Pierce <31912242+colemtg@users.noreply.github.com>
2017-11-21 18:39:14 -0600

    Finished Prim I think
    Random generator does not generate redundant edges which makes doing an MST kinda silly.

--- Prim.hs
+++ Prim.hs
@@ -15,10 +15,38 @@ toString :: [UndirectedGraph] -> [String]
 toString (a:as) = show a : toString as
 toString [] = []
 
-traversePrim :: UndirectedGraph -> [V] ->[E] -> [UndirectedGraph]
+traversePrim :: UndirectedGraph -> Done->FinalEdges -> [UndirectedGraph]
 traversePrim (UndirectedGraph a b) [] e = (UndirectedGraph [] []) :
   (traversePrim (UndirectedGraph a b) ([(firstV (findMinEdge b (minEdge b (getCost (head b)) ) )),(secondV (findMinEdge b (minEdge b (getCost (head b)) ) ))]) ([findMinEdge b (minEdge b (getCost (head b)) ) ]))
-traversePrim (UndirectedGraph a b) v e = [(UndirectedGraph v e)]
+traversePrim (UndirectedGraph a b) v e = if (doneTraversal (UndirectedGraph a b) v) then [(UndirectedGraph v e)] else (UndirectedGraph v e) : (traversePrim (UndirectedGraph a b) (v ++ [(firstV (findMinEdge (findEdges (UndirectedGraph a b) v e) (minEdge (findEdges (UndirectedGraph a b) v e) (getCost (head (findEdges (UndirectedGraph a b) v e))) ) )),(secondV (findMinEdge (findEdges (UndirectedGraph a b) v e) (minEdge (findEdges (UndirectedGraph a b) v e) (getCost (head (findEdges (UndirectedGraph a b) v e))) ) ))])  (e ++[findMinEdge (findEdges (UndirectedGraph a b) v e) (minEdge (findEdges (UndirectedGraph a b) v e) (getCost (head (findEdges (UndirectedGraph a b) v e))) ) ]))
+
+
+doneTraversal :: UndirectedGraph -> Done -> Bool
+doneTraversal (UndirectedGraph a b) v =if (verticesInDone a v) then True else False
+
+verticesInDone :: [V] -> Done -> Bool
+verticesInDone (v:[]) done = if (vertexInDone v done) then True else False
+verticesInDone (v:vs) done = if (vertexInDone v done && verticesInDone vs done) then True else False
+verticesInDone [] _ = False
+
+vertexInDone :: V -> Done -> Bool
+vertexInDone v (vd:[]) = if (v == vd) then True else False
+vertexInDone v (vd:vds) = if (v == vd || vertexInDone v vds) then True else False
+vertexInDone _ [] = False
+
+findEdges :: UndirectedGraph -> Done -> FinalEdges -> [E]
+findEdges (UndirectedGraph a b) v e = newEdges b e
+
+newEdges :: [E] ->FinalEdges -> [E]
+newEdges (e:es) fEs = if (edgeInDone e fEs) then  (newEdges es fEs) else e: (newEdges es fEs)
+newEdges [] _  = []
+
+edgeInDone :: E ->FinalEdges -> Bool
+edgeInDone e (fe:fes) = if ((edgesAreEqual e fe) || (edgeInDone e fes)) then True else False
+edgeInDone e [] = False
+
+edgesAreEqual :: E -> E -> Bool
+edgesAreEqual (ca , v1a, v2a) (cb, v1b ,v2b) = if (ca == cb && v1a == v1b && v2a == v2b) then True else False
 
 firstV :: E -> V
 firstV (_,v,_) = v
@@ -32,7 +60,10 @@ minEdge (e:es) min  = if (min > getCost e) then minEdge es (getCost e)
                                     else minEdge es (min)
 
 findMinEdge :: [E] -> Int -> E
-findMinEdge (e:es) min = if (getCost e == min) then e else findMinEdge es min 
+findMinEdge [] _ = (100 , C.empty, C.empty)
+findMinEdge (e:[]) _ = e
+findMinEdge (e:es) min = if (getCost e == min) then e else findMinEdge es min
+
 
 getCost :: E -> Int
 getCost (cost, _, _) = cost
