25432dda679b2d994b3b5c7887736fd4a76faa55
Cole Pierce <31912242+colemtg@users.noreply.github.com>
2017-12-04 10:53:34 -0600

    Did similar traversal as Prim, need logic for adding or no

--- Kruskal.hs
+++ Kruskal.hs
@@ -7,29 +7,40 @@ import qualified Data.Trie as D
 import qualified Data.ByteString.Char8 as C
 
 
-kruskal :: UndirectedGraph -> [String]
+kruskalTxt :: UndirectedGraph -> [String]
 --kruskal a = [show a] {- initial graph -}
-kruskal (UndirectedGraph a b) = show (UndirectedGraph a b) : toString (traverseKruskal (UndirectedGraph a (sortE b)) 0 [] [])
+kruskalTxt (UndirectedGraph a b) = show (UndirectedGraph a b) : toString (traverseKruskal (UndirectedGraph a (sortE b)) 1 [] [])
+
+kruskalGv :: UndirectedGraph -> [String]
+kruskalGv (UndirectedGraph a b) = vizHelper (UndirectedGraph a b) : toGraphViz (traverseKruskal (UndirectedGraph a (sortE b)) 1 [] [])
 
 --This sorts the edges in Ascending order
 sortE :: [E] -> [E]
 sortE e = sort e
 
---not done
+--Traverses through each edge, adding only if helpful
 traverseKruskal :: UndirectedGraph -> Int -> Done -> FinalEdges -> [UndirectedGraph]
-traverseKruskal (UndirectedGraph a b) n [] e =  (UndirectedGraph [] []) : if ((getNextHelpfulEdge b [] n) == 0) then [] else traverseKruskal (UndirectedGraph a b) ((getNextHelpfulEdge b [] n) + 1) [] []
+traverseKruskal (UndirectedGraph a b) n [] f =  (UndirectedGraph [] []) :  traverseKruskal (UndirectedGraph a b) ((getNextHelpfulEdge b [] f n) + 1) (addVs (getEdgeAtN b (getNextHelpfulEdge b [] f n)) []) (f ++ [(getEdgeAtN b (getNextHelpfulEdge b [] f n))])
+traverseKruskal (UndirectedGraph a b) n d f = (UndirectedGraph d f) : if ((getNextHelpfulEdge b d f n) == 0) then [] else  traverseKruskal (UndirectedGraph a b) ((getNextHelpfulEdge b d f n) + 1) (d ++ (addVs (getEdgeAtN b (getNextHelpfulEdge b d f n)) d))  (f ++ [(getEdgeAtN b (getNextHelpfulEdge b [] f n))])
 
-getNextHelpfulEdge :: [E] -> Done ->Int -> Int
-getNextHelpfulEdge e done n = if (length e)<n then 0 else if edgeHelpful (getEdgeAtN e n) done then n else getNextHelpfulEdge e done (n+1)
+--given location in [E] give the next helpful edge or 0 if done
+getNextHelpfulEdge :: [E] -> Done -> FinalEdges ->Int -> Int
+getNextHelpfulEdge e done f n = if (length e)<n then 0 else if edgeHelpful (getEdgeAtN e n)  done f then n else getNextHelpfulEdge e done f (n+1)
 
+--take and E and add Vs if not already in Done
+addVs :: E -> Done -> [V]
+addVs (_, v1, v2) done = if(vertexInDone v1 done && vertexInDone v2 done) then [] else if(vertexInDone v1 done) then [v2] else if (vertexInDone v2 done) then [v1] else [v1,v2]
 
+--get an E for array of edges ar location n
 getEdgeAtN ::  [E] -> Int -> E
-getEdgeAtN (e:es) 0 = e
+getEdgeAtN (e:es) 1 = e
+getEdgeAtN (e:[]) n = e
 getEdgeAtN (e:es) n = getEdgeAtN es (n-1)
 
+
 --not done
-edgeHelpful :: E -> Done -> Bool
-edgeHelpful e done = True
+edgeHelpful :: E -> Done -> FinalEdges -> Bool
+edgeHelpful e done f = True
 
 
-                                                         
+                                                        
